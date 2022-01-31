b2a31fe6dfc339388c575a21075eacb4a8694761
Eduardo Costa <ecosta@hubspot.com>
2022-01-31 18:37:58 +0000

    reading stuff from disk

--- mbr.s
+++ mbr.s
@@ -1,15 +1,24 @@
   .code16
 start:
   cli
+  xorw %ax, %ax
+  movw %ax, %ss
+  movw $0x7c00, %sp
+
+  movw $0x0201, %ax
+  movw $0x0002, %cx
+  movw $0x0080, %dx
+
   movw $0xb800, %bx
   movw %bx, %es
-  xorw %di, %di
-  movb $0x40, %al
-  movw $10, %cx
-  rep stosb
+  xorw %bx, %bx
+  int $0x13
+
 halt:
   hlt
   jmp halt
 
 .org 510
 .short 0xAA55
+
+.string "H@e@l@l@o@ @!@!@              "
